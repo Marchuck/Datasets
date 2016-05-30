61e7261e975696f9cfff853568ba239e74f82f07
Lukasz <lukmar993@gmail.com>
2016-05-30 14:35:54 +0200

    added new dialog

--- src/pl/datasets/custom_widgets/PanelRenderer.java
+++ src/pl/datasets/custom_widgets/PanelRenderer.java
@@ -14,6 +14,7 @@ public class PanelRenderer<T extends Nameable> implements ListCellRenderer<T> {
     @Override
     public Component getListCellRendererComponent(JList<? extends T> list, T value, int index, boolean isSelected, boolean cellHasFocus) {
         JLabel label = new JLabel(value.getName(), JLabel.LEFT);
+
         if (isSelected) label.setForeground(Pallete.primaryColor());
         else label.setForeground(Pallete.accentColor());
         return label;
