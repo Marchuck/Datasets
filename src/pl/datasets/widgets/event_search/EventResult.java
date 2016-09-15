4de6427a322bb4ef9beee0968a382bdb406c1b2d
Johannes Planis <johannes.planis@gmail.com>
2016-09-15 19:03:06 +0200

    rozpoczęcie pracy nad zwracaniem większej ilości danych do raportu

---
+++ src/pl/datasets/widgets/event_search/EventResult.java
@@ -0,0 +1,12 @@
+package pl.datasets.widgets.event_search;
+
+import pl.datasets.utils.Event;
+
+/**
+ * Created by JOHANNES on 9/15/2016.
+ */
+public class EventResult {
+
+    private Event event;
+
+}
